6213ee1a807d74b12c68b807c44a111df421a38c
Jason Cassella <jasoncassella1@gmail.com>
2022-08-06 13:44:44 -0400

    created public and views folders

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ MongoClient.connect(dbConnectionString)
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
