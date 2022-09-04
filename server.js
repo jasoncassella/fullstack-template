0068bf83f2851984c3a4d3fce136aa9b61dbd6eb
Jason Cassella <jasoncassella1@gmail.com>
2022-09-04 14:53:21 -0400

    dependencies installed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 require('dotenv').config();
 
 let db,
